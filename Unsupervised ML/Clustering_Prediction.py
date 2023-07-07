44199af1251ac1bd952a5923d5690b8b885f457d
fdletter1980 <83425433+fdletter1980@users.noreply.github.com>
2023-07-07 08:18:04 -0400

    Update Clustering_Prediction.py

--- Unsupervised ML/Clustering_Prediction.py
+++ Unsupervised ML/Clustering_Prediction.py
@@ -10,13 +10,13 @@ with open(pkl_filename, 'rb') as file:
     clustering_model = pickle.load(file)
 
 
-print("Enter the following details to make the predictions:- n")
+print("Enter the following details to make the predictions:")
 
 Avg_Credit_Limit = int(input("Enter The Average Credit Limit:- "))
-Total_Credit_Cards = int(intput("Enter Total # of Credit Cards:- "))
-Total_visits_bank = int(intput("Enter Total Visits to Bank:- "))
-Total_visits_online = int(intput("Enter Total Visits Online:- "))
-Total_calls_made = int(intput("Enter Total Calls Made:- "))
+Total_Credit_Cards = int(input("Enter Total # of Credit Cards:- "))
+Total_visits_bank = int(input("Enter Total Visits to Bank:- "))
+Total_visits_online = int(input("Enter Total Visits Online:- "))
+Total_calls_made = int(input("Enter Total Calls Made:- "))
 
 customer_prediction = clustering_model.predict([[Avg_Credit_Limit,Total_Credit_Cards,Total_visits_bank,Total_visits_online,Total_calls_made]])
 
